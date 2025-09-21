46200c6b05938242d557b91e9aa3993ed0969b67
Huseyin DOL <huseyindol@gmail.com>
2025-09-22 02:48:50 +0300

    home page servis type tanımlaması

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,13 +1,14 @@
 import Hero from '@/components/Hero'
 import Skills from '@/components/Skills'
 import Experience from '@/components/experience'
+import { APIResponseSuccessType } from '@/types/APITypes'
 import { formatMetadata } from '@/utils'
 import { fetcher } from '@/utils/services/fetcher'
 import { Page, PageSEO } from '@prisma/client'
 import { Metadata } from 'next'
 
 export async function generateMetadata(): Promise<Metadata> {
-  const page = await fetcher(
+  const page: APIResponseSuccessType<Page> = await fetcher(
     `${process.env.NEXT_PUBLIC_HOST}/api/page?slug=home`,
     {
       next: {
@@ -15,8 +16,9 @@ export async function generateMetadata(): Promise<Metadata> {
       },
     },
   )
+
   const formatMeta = formatMetadata(
-    page as unknown as Page & { pageSEO: PageSEO },
+    page.data as unknown as Page & { pageSEO: PageSEO },
   )
   return {
     ...formatMeta,
